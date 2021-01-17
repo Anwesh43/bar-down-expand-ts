5bfacd7e02f305c9e84adf3c1325ea1197932d97
Anwesh43 <anweshthecool0@gmail.com>
2021-01-17 21:35:50 +0530

    adding BarDownExpand class

--- index.ts
+++ index.ts
@@ -170,3 +170,26 @@ class BDENode {
         return this 
     }
 }
+
+class BarDownExpand {
+
+    curr : BDENode = new BDENode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+    
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
