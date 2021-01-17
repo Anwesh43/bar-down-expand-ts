a2ffbcd481dcb346fd7c700429a2c4aecd6000ca
Anwesh43 <anweshthecool0@gmail.com>
2021-01-17 21:19:43 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -53,3 +53,34 @@ class DrawingUtil {
         DrawingUtil.drawBarDownExpand(context, scale)
     }
 }
+
+class Stage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor 
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static handleTap() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
